380e421b37d674514a1b8b845ece56c40e7d39f6
John Shahid <jvshahid@gmail.com>
2016-10-21 13:40:51 -0400

    cco/cflager -> cco/lager/lagerflags
    [#125073871]
    
    Signed-off-by: Nima Kaviani <nkavian@us.ibm.com>

--- simulation/repnode/main.go
+++ simulation/repnode/main.go
@@ -6,8 +6,8 @@ import (
 	"log"
 
 	"code.cloudfoundry.org/auction/simulation/simulationrep"
-	"code.cloudfoundry.org/cflager"
 	executorfakes "code.cloudfoundry.org/executor/fakes"
+	"code.cloudfoundry.org/lager/lagerflags"
 	"code.cloudfoundry.org/rep"
 	"code.cloudfoundry.org/rep/evacuation/evacuation_context/fake_evacuation_context"
 	rephandlers "code.cloudfoundry.org/rep/handlers"
@@ -26,7 +26,7 @@ var stack = flag.String("stack", "", "stack")
 var zone = flag.String("zone", "Z0", "availability zone")
 
 func main() {
-	cflager.AddFlags(flag.CommandLine)
+	lagerflags.AddFlags(flag.CommandLine)
 	flag.Parse()
 
 	if *repGuid == "" {
@@ -43,7 +43,7 @@ func main() {
 		Containers: *containers,
 	}, []string{})
 
-	logger, _ := cflager.New("repnode-http")
+	logger, _ := lagerflags.New("repnode-http")
 
 	fakeExecutorClient := new(executorfakes.FakeClient)
 	fakeEvacuatable := new(fake_evacuation_context.FakeEvacuatable)
